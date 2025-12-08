45e252d8e1ce1130ca3e12ca931d5f23be5c5fcd
Gaston <gastons525@gmail.com>
2025-12-08 14:33:23 -0300

    cas

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PedidoProvider } from "@/context/pedidoContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
